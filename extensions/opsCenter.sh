c723f4e18ae2958e5c6c12d2594258d7f9a02096
Gustavo Arjones <garjones@socialmetrix.com>
2016-09-09 11:54:53 -0300

    WIP: commenting install script - trying manual install

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -15,14 +15,14 @@ echo "Calling opscenter.sh with the settings:"
 echo cloud_type $cloud_type
 echo seed_node_dns_name $seed_node_dns_name
 
-apt-get -y install unzip
+# apt-get -y install unzip
 
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-5.zip
-unzip 5.0.1-5.zip
-cd install-datastax-ubuntu-5.0.1-5/bin
+# wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-5.zip
+# unzip 5.0.1-5.zip
+# cd install-datastax-ubuntu-5.0.1-5/bin
 
-#wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
-#unzip master.zip
-#cd install-datastax-ubuntu-master/bin
+# #wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
+# #unzip master.zip
+# #cd install-datastax-ubuntu-master/bin
 
-./opscenter.sh $cloud_type $seed_node_dns_name
+# ./opscenter.sh $cloud_type $seed_node_dns_name
--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-RESOURCE_GROUP=$1
+azure group create --tags 'billing=quantum' datastax eastus2
 
-azure group create $1 "eastasia"
-azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
+azure group deployment create \
+  --template-file mainTemplate.json \
+  --parameters-file mainTemplateParameters.json \
+  datastax datastax
