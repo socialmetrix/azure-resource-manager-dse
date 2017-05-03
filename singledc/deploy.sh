8499c189698936942c36fda2f9701e0a869903d2
Maximiliano Felice <maximilianofelice@gmail.com>
2017-05-03 17:09:25 -0300

    WIP: Configuring New Analytics DC

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-azure group create --tags 'billing=quantum' datastax eastus2
+# azure group create --tags 'billing=quantum' datastax eastus2
 
 azure group deployment create \
   --template-file mainTemplate.json \
