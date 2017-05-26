8aac67cb689f49907bcdc1927b0c24682409030d
David <rodavidfernandez@gmail.com>
2017-05-26 12:33:26 -0300

    edit values

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-azure group create --tags 'billing=quantum' datastax eastus2
+azure group create --tags 'billing=quantum' test eastus2
 
 azure group deployment create \
   --template-file mainTemplate.json \
   --parameters-file mainTemplateParameters.json \
-  datastax datastax
+  test  test
