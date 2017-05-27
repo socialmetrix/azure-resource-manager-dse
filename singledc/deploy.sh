42902a2892662979ffd391387f77338437d4eff1
Gustavo Arjones <garjones@socialmetrix.com>
2017-05-27 01:28:36 +0000

    Cosmetic changes

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-azure group create --tags 'billing=quantum' datastax eastus2
+DATACENTER_NAME=rationale
+
+azure group create --tags 'billing=quantum' ${DATACENTER_NAME} eastus2
 
 azure group deployment create \
   --template-file mainTemplate.json \
   --parameters-file mainTemplateParameters.json \
-  datastax datastax
+  ${DATACENTER_NAME} ${DATACENTER_NAME}
